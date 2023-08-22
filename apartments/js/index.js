7d21368d280029c8888f54cb1a956a789ba211e2
t-hasic <122468022+t-hasic@users.noreply.github.com>
2023-08-22 15:56:29 -0400

    first commit

---
+++ apartments/js/index.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // location = document.getElementById('desired-location');
+
+
+    // search.onload() = {
+    //     num_rooms = localStorage.getItem("");
+
+
+    //     city = localStorage.setItem("city");
+    //     price = localStorage.setItem("price", price);
+    //     num_rooms = localStorage.setItem("num_rooms", num_rooms);
+    // };
+});
